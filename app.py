5b8dc26f95f5cc16e4fc60a1f2200e5701173f79
Xánder Cárdenas <xandercj@gmail.com>
2023-04-02 14:13:36 -0500

    Tercer commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Cancion, Usuario, Album, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -10,9 +10,22 @@ db.create_all()
 
 #PRUEBA
 with app.app_context():
+    
+    #c2 = Cancion(titulo="Prueba2", minutos=10,segundos=25, interprete='Gina' )
+    u = Usuario(nombre='Juan', contrasena='123456')
+    a = Album(titulo='Prueba',anio=199, descripcion='texto', medio=Medio.CD)
     c = Cancion(titulo="Prueba", minutos=2,segundos=25, interprete='Xander' )
-    c2 = Cancion(titulo="Prueba2", minutos=10,segundos=25, interprete='Gina' )
+    u.albumes.append(a)
+    a.canciones.append(c)
+    db.session.add(u)
     db.session.add(c)
-    db.session.add(c2)
     db.session.commit()
+    
+    #db.session.add(c2)
+    print(Usuario.query.all())
+    print(Album.query.all())
+    print(Album.query.all()[0].canciones)
+    print(Cancion.query.all())
+    db.session.delete(a)
+    print(Album.query.all())
     print(Cancion.query.all())
