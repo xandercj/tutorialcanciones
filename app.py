d45aa1af3196a8469d4e62c34df5e5652843b969
Xánder Cárdenas <xandercj@gmail.com>
2023-04-02 12:59:36 -0500

    Segundo commit

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flaskr import create_app
+from .modelos import db, Cancion
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+#PRUEBA
+with app.app_context():
+    c = Cancion(titulo="Prueba", minutos=2,segundos=25, interprete='Xander' )
+    c2 = Cancion(titulo="Prueba2", minutos=10,segundos=25, interprete='Gina' )
+    db.session.add(c)
+    db.session.add(c2)
+    db.session.commit()
+    print(Cancion.query.all())
